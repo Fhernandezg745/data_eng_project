34253884fc4ae51bfaafe03e01b32de9acec1701
Fhernandezg745 <fer.hndez@gmail.com>
2023-07-31 20:47:03 -0300

    archivos entregables

--- entregable_api_alphavantage.py
+++ entregable_api_alphavantage.py
@@ -7,7 +7,12 @@ from typing import Union, List
 import pandas as pd
 from dotenv import load_dotenv
 import sqlalchemy as sa
-load_dotenv()
+
+#Autenticacion y url base de API Alphavantage
+#Configuro los parametros de la API desde un archivo .env, 
+base_url = os.environ.get('BASE_URL')
+token = os.environ.get('API_TOKEN')
+
 
 #Creo una clase para agarrar los errores que puedan venir en la API
 class APIRequestError(Exception):
@@ -89,7 +94,7 @@ def getSentiment(
 
 # 3 Unifico las funciones de market data y news data en una sola y creo las tablas en redshift
 def get_stock_data(tickers, interval, topics):
-    successful_rows = {}
+    stock_data_frames = {}
     # Get database connection parameters
     DB_NAME = os.environ.get('DB_NAME')
     DB_USER = os.environ.get('DB_USER')
@@ -103,7 +108,7 @@ def get_stock_data(tickers, interval, topics):
         f"postgresql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
         connect_args={'options': f'-csearch_path={dbschema}'}
     )
-    
+
     for ticker in tickers:
         try:
             # Traigo intraday stock data
@@ -122,23 +127,42 @@ def get_stock_data(tickers, interval, topics):
             intraday_table_name = f'stock_intraday_prices_{ticker}'
             
             #Creo tabla de precios en redshift
-            df_intraday.to_sql(intraday_table_name, conn, index=False, if_exists='replace')
-            # cantidad de rows de intraday 
-            num_rows_updated_intraday = len(df_intraday)
+            conn.execute(f"""
+                CREATE TABLE {intraday_table_name} (
+                    date TIMESTAMP,
+                    open_price FLOAT,
+                    high_price FLOAT,
+                    low_price FLOAT,
+                    close_price FLOAT,
+                    volume INT
+                )
+                DISTKEY(date)
+                SORTKEY(date);
+            """)
+    
 
             #Voy con el dataframe de news sentiment
             df_sentiment = pd.DataFrame(sentiment_data)
             sentiment_table_name = f'stock_sentiment_{ticker}'
             
             #Creo tabla de sentiment en redshift
-            df_sentiment.to_sql(sentiment_table_name, conn, index=False, if_exists='replace')
-            # cantidad de rows de sentiment
-            num_rows_updated_sentiment = len(df_sentiment)
+            conn.execute(f"""
+            CREATE TABLE {sentiment_table_name}  (
+                ticker VARCHAR,
+                time_published TIMESTAMP,
+                source_domain VARCHAR,
+                relevance_score VARCHAR,
+                tiker_sentiment_label VARCHAR
+            )
+            DISTKEY(time_published)
+            SORTKEY(time_published);
+            """)
 
             
-            successful_rows[ticker] = {
-                'intraday_data': num_rows_updated_intraday,
-                'sentiment_data': num_rows_updated_sentiment
+            # Guardo los df en diccionarios
+            stock_data_frames[ticker] = {
+                'intraday_data': df_intraday,
+                'sentiment_data': df_sentiment
             }
             
         except APIRequestError as api_err:
@@ -146,25 +170,26 @@ def get_stock_data(tickers, interval, topics):
             # You can handle the error based on the status code here.
             # For example, you may choose to skip the stock if the error is not recoverable.
             continue
-    return successful_rows
+    return stock_data_frames
 
 
 ################################################################
 
-#Configuro los parametros de la API desde un archivo .env, establezco que voy a querer ver stocks de Apple y de IBM en intervalo de 60min
+# Establezco que voy a querer ver stocks de Apple y de IBM en intervalo de 60min
 # La Api de advantage tiene un limit por default de 100 filas para los stockprices y 50 para news sentiment, pero puede customizarse 
 # agregandole una fecha rango o un mes
 # topics es un parametro para bucar noticias de esos temas en particular
 #interval es un parametro para establecer la frecuencia del precio de la accion que queramos, en este caso cada una hora
-base_url = os.environ.get('BASE_URL')
-token = os.environ.get('API_TOKEN')
+
 tickers = ['AAPL','IBM']
 interval = '60min'
 topics = 'technology, manufacturing, financial_markets'
 
 ################################################################
-# Creo una variable donde ejecuto la funcion y guardo el resultado
+# Creo una variable donde ejecuto la funcion y guardo los resultados como dataframes
+# La funcion get_stock_data me va a dar una tabla de 100 registros para el precio en 60 min de cada accion que esté en la lista, y ademas una tabla con
+# las ultimas 50 noticias sobre ese stock con una valoracion de sentiment analysis. Esto se va a guardar en un diccionario para cada ticker, y dentro 
+# de cada key hay 2 valores, uno de precio en 60 min de cada accion, y otro de sentiment analysis que contiene cada uno el dataframe correspondiente
 data_frames_by_ticker = get_stock_data(tickers, interval, topics)
 
-### Instancio los resultados de cada accion y de cada tabla
-print(data_frames_by_ticker)
+
